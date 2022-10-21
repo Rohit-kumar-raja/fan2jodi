d958f74411a635db48a521fb1458220739b77a72
Raja <raja@raja.com>
2022-10-21 08:23:55 +0530

    all done

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,61 @@
 <?php
 
+use App\Http\Controllers\ContestController;
+use App\Http\Controllers\indexController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\WalletController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [indexController::class, 'index'])->name('home');
+Route::get('contest/{matche_id}', [ContestController::class, 'index'])->name('contest');
+Route::get('wallet/transaction', [WalletController::class, 'transaction'])->name('wallet.transaction');
+
+// user management
+Route::post('user/register', [LoginController::class, 'register'])->name('user.register');
+Route::post('user/login', [LoginController::class, 'login'])->name('user.login');
+// user management
+
+
+Route::get('register', function () {
+    return view('register');
+})->name('register');
+
+Route::get('login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('player', function () {
+    return view('player');
+})->name('player');
+
+Route::get('price_list', function () {
+    return view('price_list');
+})->name('price_list');
+
+Route::get('wallet', function () {
+    return view('wallet');
+})->name('wallet');
+
+Route::get('add_cash', function () {
+    return view('add_cash');
+})->name('add_cash');
+
+Route::get('account', function () {
+    return view('account');
+})->name('account');
+
+
+Route::get('game', function () {
+    return view('game');
+})->name('game');
+
+
+
+Route::get('list', function () {
+    return view('list');
+})->name('list');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
