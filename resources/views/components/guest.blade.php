2ec8ecf10de978016ada5b8a1345b8a2010168d6
Raja <raja@raja.com>
2022-10-22 10:49:59 +0530

    all den

--- resources/views/components/guest.blade.php
+++ resources/views/components/guest.blade.php
@@ -1,52 +1,54 @@
 <!doctype html>
 <html lang="zxx">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Links of CSS files -->
-        <link rel="stylesheet" href="{{url('css/bootstrap.min.css')}}">
-        <link rel="stylesheet" href="{{url('css/animate.min.css')}}">
-        <link rel="stylesheet" href="{{url('css/boxicons.min.css')}}">
-        <link rel="stylesheet" href="{{url('css/meanmenu.min.css')}}">
-        <link rel="stylesheet" href="{{url('css/flaticon.css')}}">
-        <link rel="stylesheet" href="{{url('css/fancybox.min.css')}}">
-        <link rel="stylesheet" href="{{url('css/owl.carousel.min.css')}}">
-        <link rel="stylesheet" href="{{url('css/owl.theme.default.min.css')}}">
-        <link rel="stylesheet" href="{{url('css/magnific-popup.min.css')}}">
-        <link rel="stylesheet" href="{{url('css/style.css')}}">
-        <link rel="stylesheet" href="{{url('css/responsive.css')}}">
-        <link rel="stylesheet" href="{{url('css/dark-style.css')}}">
-        <link rel="stylesheet" href="{{url('css/custom.css')}}">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>Cricket - {{$title}}</title>
+    <!-- Links of CSS files -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/boxicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/meanmenu.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/fancybox.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/magnific-popup.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dark-style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 
-        <link rel="icon" type="image/png" href="{{url('img/favicon.png')}}">
-    </head>
+    <title>Cricket - {{ $title }}</title>
 
-    <body>
+    <link rel="icon" type="image/png" href="{{ url('img/favicon.png') }}">
+</head>
+
+<body>
 
     {{-- @include('include.header') --}}
-    {{$body}}
+    {{ $body }}
     {{-- @include('include.footer') --}}
-    </body>
-      <!-- Links of JS files -->
-      {{-- <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js')}}"></script> --}}
-      <script src="{{url('js/jquery.min.js')}}"></script>
-      <script src="{{url('js/bootstrap.bundle.min.js')}}"></script>
-      <script src="{{url('js/owl.carousel.min.js')}}"></script>
-      <script src="{{url('js/magnific-popup.min.js')}}"></script>
-      <script src="{{url('js/parallax.min.js')}}"></script>
-      <script src="{{url('js/meanmenu.min.js')}}"></script>
-      <script src="{{url('js/isotope.pkgd.min.js')}}"></script>
-      <script src="{{url('js/cursor.min.js')}}"></script>
-      <script src="{{url('js/fancybox.min.js')}}"></script>
-      <script src="{{url('js/wow.min.js')}}"></script>
-      <script src="{{url('js/form-validator.min.js')}}"></script>
-      <script src="{{url('js/contact-form-script.js')}}"></script>
-      <script src="{{url('js/ajaxchimp.min.js')}}"></script>
-      <script src="{{url('js/main.js')}}"></script>
-      <script src="{{url('js/test.js')}}"></script>
-  </body>
+</body>
+<!-- Links of JS files -->
+{{-- <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js')}}"></script> --}}
+<script src="{{ url('js/jquery.min.js') }}"></script>
+<script src="{{ url('js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ url('js/owl.carousel.min.js') }}"></script>
+<script src="{{ url('js/magnific-popup.min.js') }}"></script>
+<script src="{{ url('js/parallax.min.js') }}"></script>
+<script src="{{ url('js/meanmenu.min.js') }}"></script>
+<script src="{{ url('js/isotope.pkgd.min.js') }}"></script>
+<script src="{{ url('js/cursor.min.js') }}"></script>
+<script src="{{ url('js/fancybox.min.js') }}"></script>
+<script src="{{ url('js/wow.min.js') }}"></script>
+<script src="{{ url('js/form-validator.min.js') }}"></script>
+<script src="{{ url('js/contact-form-script.js') }}"></script>
+<script src="{{ url('js/ajaxchimp.min.js') }}"></script>
+<script src="{{ url('js/main.js') }}"></script>
+<script src="{{ url('js/test.js') }}"></script>
+</body>
+
 </html>
